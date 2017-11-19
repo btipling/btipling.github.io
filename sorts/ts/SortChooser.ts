a62c0e6c8718727516a111239a1e3ff463a7441c
Bjorn Tipling <bjorn@imgur.com>
2017-11-19 14:23:00 -0800

    More cleanup.

--- sorts/ts/SortChooser.ts
+++ sorts/ts/SortChooser.ts
@@ -3,20 +3,14 @@ import {
     div,
     p,
 } from '@cycle/dom';
-import { Location } from 'history';
 import xs from 'xstream';
 import { ISinks, ISources } from './typedefs';
 
-export default function SortChooser(sources: ISources): ISinks {
-    const history$ = sources.history;
-
-    const vdom$ = history$.map((path: Location) => {
-        console.log(path);
-        return div([
-            p('Choose a sort.'),
-            a({ props: { href: './bubblesort' } }, 'bubble sort'),
-        ]);
-    });
+export default function SortChooser(_: ISources): ISinks {
+    const vdom$ = xs.of(div([
+        p('Choose a sort.'),
+        a({ props: { href: './bubblesort' } }, 'bubble sort'),
+    ]));
 
     return {
         dom: vdom$,
