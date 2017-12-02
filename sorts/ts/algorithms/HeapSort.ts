2436af343c62bdd5709e206ce55579f7a38c509c
Bjorn Tipling <bjorn@imgur.com>
2017-12-01 23:14:15 -0800

    Pretty happy with the heap sort visualization now.

--- sorts/ts/algorithms/HeapSort.ts
+++ sorts/ts/algorithms/HeapSort.ts
@@ -6,7 +6,7 @@ interface INode { left: INode | null; right: INode | null; value: number; }
 function addToHeap(nodeToAdd: INode, currentNode: INode) {
     if (nodeToAdd.value <= currentNode.value) {
         if (currentNode.left) {
-            addToHeap(nodeToAdd, currentNode.left);
+            return addToHeap(nodeToAdd, currentNode.left);
         } else {
             currentNode.left = nodeToAdd;
         }
@@ -16,9 +16,49 @@ function addToHeap(nodeToAdd: INode, currentNode: INode) {
         } else {
             currentNode.right = nodeToAdd;
         }
-
     }
-    return currentNode;
+}
+
+function findArrayPositionForNode(haystack: INode, needle: INode, currentPosition: number = 0): { position: number, found: boolean } {
+    /*
+    position starts at zero
+    we go into the function
+    we check if the head is the node if so return found with position 0
+    if not we increment one and check left
+        - we go into the function
+        - we check if the new head is the node and if so return found with position 1
+            - we go to the top and see if found return 1 and that's done
+        - or we check the right and increase the position to 2
+            - we go into the function
+            - we check if the new head is the node and if so return found with position 2
+                - we return to the first left and see found from right and return 2
+                    - we return to the top and see found from left and return 2 and that's done
+            - or we set position to 3 and check the left
+    */
+    let position = currentPosition;
+    let result: { position: number, found: boolean } | null = null;
+    if (haystack.left) {
+        result = findArrayPositionForNode(haystack.left, needle, position);
+        if (result.found) {
+            return result;
+        } else {
+            position = result.position + 1;
+        }
+    }
+    if (haystack === needle) {
+        return { position, found: true };
+    }
+    if (haystack.right) {
+        // Increment position on going right.
+        result = findArrayPositionForNode(haystack.right, needle, position + 1);
+        if (result.found) {
+            return result;
+        } else {
+            position = result.position;
+        }
+    }
+    // Increment position on coming up.
+    return { found: false, position };
 }
 
 function heapToSortedArray(currentNode: INode, arr: number[]) {
@@ -46,10 +86,20 @@ export function* heapSort(unsortedArray: number[], makeSortData: MakeSortDataFun
         if (!arr.length) {
             return;
         }
-        addToHeap({ left: null, right: null, value: arr[0] as number }, head);
+        const currentIndex = unsortedArray.length - arr.length;
         yield makeSortData(
-            makeSortDemoData(unsortedArray, -1, [], []),
+            makeSortDemoData(trackingArray, -1, [currentIndex], []),
             makeSortDemoData(fix(heapToSortedArray(head, []), unsortedArray.length), -1, [], []),
+        );
+        const newNode = { left: null, right: null, value: arr[0] as number };
+        addToHeap(newNode, head);
+        const findResult = findArrayPositionForNode(head, newNode);
+        const indexAddedAt = findResult.position;
+        const currentSortedProgress = heapToSortedArray(head, []);
+        trackingArray[currentIndex] = 0;
+        yield makeSortData(
+            makeSortDemoData(trackingArray, -1, [], []),
+            makeSortDemoData(fix(currentSortedProgress, unsortedArray.length), -1, [indexAddedAt], []),
         );
         yield* feedHeap(arr.slice(1, arr.length), head) as any;
         return;
@@ -60,6 +110,8 @@ export function* heapSort(unsortedArray: number[], makeSortData: MakeSortDataFun
         makeSortDemoData([], -1, [], []),
     );
     const headNode = { left: null, right: null, value: unsortedArray[0] as number };
+    const trackingArray = ([] as number[]).concat(unsortedArray);
+    trackingArray[0] = -1;
     yield* feedHeap(unsortedArray.slice(1, unsortedArray.length), headNode) as any;
     const sortedArray = heapToSortedArray(headNode, []);
 
