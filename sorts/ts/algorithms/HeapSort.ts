df31198c05034224eda7520d4e263bdc211406bb
Bjorn Tipling <bjorn@imgur.com>
2017-12-02 22:34:34 -0800

    Heapsort count.

--- sorts/ts/algorithms/HeapSort.ts
+++ sorts/ts/algorithms/HeapSort.ts
@@ -120,13 +120,55 @@ function genSort(scale: number, makeSortData: MakeSortDataFunc): ISorter {
 
 function heapSortOpCounter(scale: number): number {
     let count = 0;
-    const len = randArrayOfNumbers(scale).length;
-    for (let i = len; i > 0; i--) {
-        for (let j = 0; j < i - 1; j++) {
-            count += 1;
+    const unsortedArray = randArrayOfNumbers(scale);
+
+    function feedHeap(arr: number[], head: INode) {
+        if (!arr.length) {
+            return;
+        }
+        const newNode = { left: null, right: null, value: arr[0] as number };
+        addToHeapAndCount(newNode, head);
+        feedHeap(arr.slice(1, arr.length), head);
+        return;
+    }
+
+    const headNode = { left: null, right: null, value: unsortedArray[0] as number };
+    feedHeap(unsortedArray.slice(1, unsortedArray.length), headNode);
+    heapToSortedArrayAndCount(headNode, []);
+    return count;
+
+    function addToHeapAndCount(nodeToAdd: INode, currentNode: INode) {
+        count += 1;
+        if (nodeToAdd.value <= currentNode.value) {
+            if (currentNode.left) {
+                return addToHeapAndCount(nodeToAdd, currentNode.left);
+            } else {
+                currentNode.left = nodeToAdd;
+            }
+        } else {
+            if (currentNode.right) {
+                addToHeapAndCount(nodeToAdd, currentNode.right);
+            } else {
+                currentNode.right = nodeToAdd;
+            }
         }
     }
-    return count;
+
+    function heapToSortedArrayAndCount(currentNode: INode, arr: number[]) {
+        if (!currentNode) {
+            return arr;
+        }
+        count += 1;
+        let result = arr;
+        if (currentNode.left) {
+            result = heapToSortedArrayAndCount(currentNode.left, result);
+        }
+        result.push(currentNode.value);
+        if (currentNode.right) {
+            result = heapToSortedArrayAndCount(currentNode.right, result);
+        }
+        return result;
+    }
 }
 
 function genSortScales(scales: number[]): number[] {
